8f879ebf771a35530fb5781576c7da7d77967c59
triihim <tatu.riihimaki@gmail.com>
2018-02-21 13:00:37 +0200

    Edited config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8080/api/';
+export const API_URL = process.env.API_URL || 'http://localhost:8080/api/';
 
 // Allowed temperature input limits in celsius degrees.
 export const MIN_TEMP = -70;
